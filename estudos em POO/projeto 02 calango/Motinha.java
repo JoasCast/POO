5803fbc9533b2c90f68ef35ad040a37d0d5d5b58
Joas Castelo <joascastelo@gmail.com>
2020-12-15 10:25:09 -0200

    Create Motinha.java

---
+++ estudos em POO/projeto 02 calango/Motinha.java
@@ -0,0 +1,44 @@
+public class Motinha {
+    int gasosa;
+    int peças;
+    boolean bater;
+    Motinha(int gasosa, int peças){
+        this.gasosa = gasosa;
+        this.peças = peças;
+        this.bater = false;
+    }
+    void mercanico(){
+        System.out.println("pausa para o reparo");
+        peças += 10;
+    }
+    void barruada(Motinha other){
+        if(other.peças >= 10){
+            System.out.println("não posso andar");
+        }
+        if(this.bater){
+        System.out.println("bati na bis");
+        other.peças -= 10;}
+    }
+    void passear(){
+        if(peças <= 10){
+            System.out.println("preciso ir no mercânico");
+        }
+        if(gasosa >=1){
+            System.out.println("dando um passeio");
+            gasosa -=1;
+        }else{
+            System.out.println("preciso abastecer");
+        }
+    }
+    void abastecer(){
+        if(gasosa >= 5){
+            System.out.println("o tanque está cheio");
+        }else{
+            gasosa+=1;
+        }
+    }
+    public static void main(String[] args) {
+        Motinha Pop = new Motinha(5, 50);
+        Motinha bis = new Motinha(5, 50);
+    }
+}
